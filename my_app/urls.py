54704b566f5e4ed10c5974b95236a0f669e3c72b
Artem <ionov96artem@mail.com>
2025-04-13 15:03:53 +0400

    Create update_ad (view, url and html) and improve navi (CSS)

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ad_list, create_ad, delete_ad
+from .views import ad_list, create_ad, delete_ad, update_ad
 
 urlpatterns = [
     path('', ad_list, name='ad_list'),
     path('create/', create_ad, name='create_ad'),
     path('delete/<int:id>', delete_ad, name='delete_ad'),
+    path('update/<int:id>', update_ad, name='update_ad'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -18,6 +18,18 @@ def create_ad(request):
         form = AdForm()                                         # Если метод запроса не POST, выводим пустую форму
     return render(request, 'create_ad.html', {'form': form})
 
+def update_ad(request, id):
+    "Редактирование объявления"
+    ad = Ad.objects.get(id=id)                                  # Получение объекта объявления с помощью id, переданного из шаблона
+    if request.method == "POST":                                # Если метод запроса POST, проверяем валидность данных
+        form = AdForm(request.POST, request.FILES, instance=ad) # Данные, введённые в поля формы, загруженные файлы, экземпляр модели
+        if form.is_valid():                                     
+            form.save()                                         # Если данные валидны, сохраняем их в базе
+            return redirect('ad_list')                          # и перенаправляем на страницу со списком объявлений
+    else:
+        form = AdForm(instance=ad)                              # Если метод запроса не POST, выводим форму c ранее сохранёнными данными
+    return render(request, 'update_ad.html', {'form': form, 'ad': ad})
+
 def delete_ad(request, id):
     "Удаление объявления"
     ad = Ad.objects.get(id=id)                                  # Получение объекта объявления с помощью id, переданного из шаблона
